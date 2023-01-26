0837519add5c484f99d0f5456d07ea8e564361ba
Marta Malapitan <martamalapitan@Martas-MacBook-Pro-2.local>
2023-01-26 00:26:44 -0800

    Create expense list dynamically based on user input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+import React, { useState } from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
 
-  const expenses = [
-    { id: 'e1', title: 'Car Insurance', amount: 200.00, date: new Date(2023, 1, 17) },
-    { id: 'e2', title: 'PG&E', amount: 250.00, date: new Date(2023, 1, 14) },
-    { id: 'e3', title: 'Mortgage', amount: 2814.00, date: new Date(2023, 1, 1) },
-    { id: 'e4', title: 'Utility', amount: 98.00, date: new Date(2023, 1, 19) },
-  ]
+  const [ expenses, setExpenses ] = useState([])
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    const expenseList = [...expenses]
+    expenseList.unshift(expense)
+
+    setExpenses(expenseList)
   }
 
   return (
